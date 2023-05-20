bbf1823663561ae81f55f2f834d1b59b1e4930ba
saiderrahmani <didi.mousedidi@outlook.com>
2023-05-20 16:23:03 -0400

    app.js

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const balanceValueEl = document.getElementById('balance-value');
 const transactionsListEl = document.getElementById('transactions-list');
+
+// Fetch transactions from API
+async function fetchTransactions() {
+    const response = await fetch('http://localhost:3000/transactions');
+    const transactions = await response.json();
+  
+    return transactions;
+  }
+  // Display transactions and balance on the page
+async function displayTransactions() {
+    const transactions = await fetchTransactions();
+    let totalIncome = 0;
+    let totalExpenses = 0;
+  
+  
