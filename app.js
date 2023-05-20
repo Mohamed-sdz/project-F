7a20b8ee8fc112b61ea9a2fb00d43201d2af41a8
saiderrahmani <didi.mousedidi@outlook.com>
2023-05-20 16:24:21 -0400

    app.js

--- app.js
+++ app.js
@@ -13,5 +13,30 @@ async function displayTransactions() {
     const transactions = await fetchTransactions();
     let totalIncome = 0;
     let totalExpenses = 0;
-  
+   
+    // Create HTML for each transaction
+  const transactionsHtml = transactions.map(transaction => {
+    const { amount, description, id, type } = transaction;
+
+    if (type === 'income') {
+      totalIncome += parseFloat(amount);
+    } else {
+      totalExpenses += parseFloat(amount);
+    }
+
+    return `<li id="transaction-${id}" class="transaction transaction-${type}">
+              <p class="transaction-description">${description}</p>
+              <p class="transaction-amount">$${amount}</p>
+              <button class="delete-button">Delete</button>
+            </li>`;
+  }).join('');
+
+  transactionsListEl.innerHTML = transactionsHtml;
+
+  // Calculate and display current balance
+  const balanceValue = totalIncome - totalExpenses;
+  balanceValueEl.textContent = `$${balanceValue.toFixed(2)}`;
+}
+
+displayTransactions();
   
